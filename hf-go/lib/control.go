29cfee20358e0a5e7e46365cae5bfa41d458406f
Jaeseung Lee <tkxkd0159@users.noreply.github.com>
2022-04-24 00:34:51 +0900

    feat: make test & benchmark case

---
+++ hf-go/lib/control.go
@@ -0,0 +1,10 @@
+package lib
+
+func ForUnicode(word string) map[string]rune {
+	wordMap := make(map[string]rune)
+	for _, r := range word {
+		wordMap[string(r)] = r
+	}
+
+	return wordMap
+}
---
+++ hf-go/lib/control_test.go
@@ -0,0 +1,40 @@
+package lib
+
+import (
+	"testing"
+)
+
+var (
+	CommonNum = 30
+	CommonStr = "Lucky"
+)
+
+func TestForUnicode(t *testing.T) {
+	var _ = CommonNum
+	var _ = CommonStr
+
+	tests := ForUnicode("abc가나다")
+	ans := []struct {
+		codePoint rune
+		char      string
+	}{
+		{97, "a"},
+		{98, "b"},
+		{99, "c"},
+		{44032, "가"},
+		{45208, "나"},
+		{45796, "다"},
+	}
+
+	for _, a := range ans {
+		if tests[a.char] != a.codePoint {
+			t.Errorf("%d is not equal to this unicode point %s", tests[a.char], a.char)
+		}
+	}
+}
+
+func BenchmarkForUnicode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ForUnicode("abc가나다")
+	}
+}
